f578b88d661e101809896058dd3f080555c32041
Marko Nikolic <nikolic89@hotmail.com>
2013-09-29 16:56:56 +0200

    Alert dialog implemented.

--- src/rs/sourcecode/loyaltyqr/CaptureActivity.java
+++ src/rs/sourcecode/loyaltyqr/CaptureActivity.java
@@ -1,14 +1,17 @@
 package rs.sourcecode.loyaltyqr;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.Handler;
-import android.widget.Toast;
 import rs.sourcecode.loyaltyqr.camera.CameraManager;
 import rs.sourcecode.loyaltyqr.camera.CaptureHandler;
 import rs.sourcecode.loyaltyqr.camera.PreviewCallback;
 import rs.sourcecode.loyaltyqr.camera.view.BoundingView;
 import rs.sourcecode.loyaltyqr.camera.view.CameraPreviewView;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Capture activity (camera barcode activity)
@@ -49,11 +52,46 @@ public class CaptureActivity extends Activity {
         super.onPause();
         cameraManager.release();
     }
+    
+    private void createResultDialog(String qrcode){
+    	
+  	  AlertDialog alertDialog = new AlertDialog.Builder(CaptureActivity.this).create(); //Read Update
+      alertDialog.setTitle("Success");
+      alertDialog.setMessage(qrcode);
+
+      alertDialog.setButton(alertDialog.BUTTON_POSITIVE, "Continue..", new DialogInterface.OnClickListener() {
+         public void onClick(DialogInterface dialog, int which) {
+            // here you can add functions
+         }
+      });
+      
+      alertDialog.setButton(alertDialog.BUTTON_NEGATIVE, "Read again", new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int which) {
+        	  // TO DO without recreating activity
+        	  reload();
+          }
+       });
+
+      alertDialog.show();
+    	
+    }
+    
+    public void reload() {
+	    Intent intent = getIntent();
+	    overridePendingTransition(0, 0);
+	    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+	    finish();
+
+	    overridePendingTransition(0, 0);
+	    startActivity(intent);
+    }   
 
     private class OnDecoded implements CaptureHandler.OnDecodedCallback {
         @Override
         public void onDecoded(String decodedData) {
-            Toast.makeText(CaptureActivity.this, decodedData, Toast.LENGTH_SHORT);
+        	//    Toast.makeText(CaptureActivity.this, decodedData, Toast.LENGTH_SHORT);
+        	createResultDialog(decodedData.toString());   	
         }
-    }
+    }   
+    
 }
